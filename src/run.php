82bf12dd94f40e46678d856a9bf25b5fd7e8a5f3
louie <louie_gk@qq.com>
2021-07-04 23:13:07 +0800

    引入config.php条件判断

--- src/run.php
+++ src/run.php
@@ -7,8 +7,9 @@ defined('IN_IA') or exit('Access Denied');
 $_config_filename = substr(__FILE__,0,strpos(__FILE__,"vendor"))."config.php";
 if(is_file($_config_filename)){
     include $_config_filename;
+} else {
+    include substr(__DIR__,0,-3)."config.php";
 }
-
 // 生成（开发）环境，报错级别
 error_reporting(E_ALL & ~E_NOTICE);
 
