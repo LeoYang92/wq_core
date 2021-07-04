dfeee3b193a4366db978ad8b64cf49cf594f8073
louie <louie_gk@qq.com>
2021-07-04 22:49:20 +0800

    引入config.php’

--- src/run.php
+++ src/run.php
@@ -4,6 +4,11 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
+$_config_filename = substr(__FILE__,0,strpos(__FILE__,"vendor"))."config.php";
+if(is_file($_config_filename)){
+    include $_config_filename;
+}
+
 // 生成（开发）环境，报错级别
 error_reporting(E_ALL & ~E_NOTICE);
 
